bafa00def4ff0c8e17924f7f3b6a42c4a417f720
Leegenux <limian22@gmail.com>
2020-04-06 12:17:48 +0800

    update to the list

---
+++ update_sorl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+FILE_NAME='OmegaRules_auto_switch.sorl'
+NOT_IN_LIST='^(update_sorl.sh|tmp)$'
+
+head -n 5 $FILE_NAME >tmp
+for filename in * ; do
+	if [[ ! $filename =~ $NOT_IN_LIST ]] ; then
+		echo $filename
+		tail -n +6 $filename >>tmp
+		rm -v $filename
+	fi
+done
+cat tmp | uniq >$FILE_NAME
+rm tmp
